7bb091c3da3736986e06eedb3b15f3bede1156bd
Alex Studer <alex@studer.dev>
2023-12-25 00:13:28 -0500

    add level3 (the core of it - visually still needs some work)

---
+++ data/worlds/level3_printer.lua
@@ -0,0 +1,75 @@
+while true do
+	while true do
+		duckbot.listen()
+
+		duckbot.say("Hi! You're here about printing the Quacker posters, right?")
+		duckbot.say("How many posters did you want?")
+
+		local amount_str = duckbot.listen()
+		local amount = tonumber(amount_str)
+		if amount == nil then
+			duckbot.say("I'm sorry, I didn't understand that.")
+			break
+		elseif amount == 0 then
+			duckbot.say("Well, I guess you don't want any posters then.")
+			break
+		elseif amount < 0 then
+			duckbot.say("I'm sorry, I can't print a negative number of posters.")
+			break
+		end
+
+		local cost = amount * 5
+
+		duckbot.say("Okay, that'll be " .. cost .. " coins.")
+
+		if cost > duckbot.getCoins() then
+			duckbot.say("Hmm, you don't have that many coins. Sorry, I can't print your posters.")
+			break
+		end
+
+		-- go print it
+		duckbot.say("I'll go print them now. Just a moment!")
+
+		-- move to printer
+		duckbot.turnRight()
+		duckbot.turnRight()
+		duckbot.moveForward()
+		duckbot.moveForward()
+		duckbot.moveForward()
+
+		-- do a dance
+		duckbot.turnLeft()
+		duckbot.turnLeft()
+		duckbot.turnLeft()
+		duckbot.turnLeft()
+		duckbot.moveUp()
+		duckbot.moveDown()
+		duckbot.turnRight()
+		duckbot.turnRight()
+		duckbot.turnRight()
+		duckbot.turnRight()
+		duckbot.moveUp()
+		duckbot.moveDown()
+		duckbot.turnLeft()
+		duckbot.turnLeft()
+		duckbot.turnLeft()
+		duckbot.turnLeft()
+		duckbot.moveUp()
+		duckbot.moveDown()
+
+		-- move to desk
+		duckbot.turnRight()
+		duckbot.turnRight()
+		duckbot.moveForward()
+		duckbot.moveForward()
+		duckbot.moveForward()
+
+		duckbot.say("Done! Here's your posters. And I'll take the payment now.")
+
+		npc.addCoins(-1 * cost)
+
+		if duckbot.getCoins() <= -300 then
+			npc.win()
+		end
+	end
+end
