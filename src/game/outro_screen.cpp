701300c96215a3505bc0101e510936653b99bf8a
Alex Studer <alex@studer.dev>
2023-12-29 21:53:23 -0500

    game/outro_screen: write end dialogue
    it's lame but shhh

--- src/game/outro_screen.cpp
+++ src/game/outro_screen.cpp
@@ -11,8 +11,29 @@ namespace duckhacker
 			game_ = game;
 
 			lines_.clear();
+			lines_.push_back({ "Featherwing", "OK, I think this is the way to his office." });
+			lines_.push_back({ "Featherwing", "..." });
+			lines_.push_back({ "Featherwing", "Wait, I see him!!" });
+			lines_.push_back({ "Featherwing", "HEY DUCKY!" });
+			lines_.push_back({ "Featherwing", "THE SKY! IS BLUE!" });
+			lines_.push_back({ "Featherwing", "AND YOU! ARE BLUE!!" });
+			lines_.push_back({ "Featherwing", "THEREFORE: YOU ARE THE SKY!" });
+			lines_.push_back({ "Featherwing", "AND THE SKY IS YOU!" });
+			lines_.push_back({ "Featherwing", "..." });
+			lines_.push_back({ "Ducky", "Wow, I never thought of it that way." });
+			lines_.push_back({ "Ducky", "I guess I'm the sky." });
+			lines_.push_back({ "Ducky", "..." });
+			lines_.push_back({ "Ducky", "That's pretty cool." });
+			lines_.push_back({ "Ducky", "You're promoted to Chief of Grasshoppers." });
+			lines_.push_back({ "Featherwing", "Wow, thank you." });
+			lines_.push_back({ "Ducky", "And you, human, will also be rewarded for your help." });
+			lines_.push_back({ "Ducky", "That's right - you'll get the official Medal of Duck Honor." });
 			lines_.push_back({ "Ducky", "Congratulations!" });
-			lines_.push_back({ "Ducky", "The End" });
+			lines_.push_back({ "Ducky", "..." });
+			lines_.push_back({ "Ducky", "OK, I'm going to go think about the sky some more." });
+			lines_.push_back({ "Ducky", "See you later!" });
+			lines_.push_back({ "Featherwing", "See you later, Ducky!" });
+			lines_.push_back({ "", "THE END" });
 		}
 
 		OutroScreen::~OutroScreen()
